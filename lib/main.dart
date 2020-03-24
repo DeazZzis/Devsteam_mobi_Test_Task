ad46d852f20788ab976f11a65bd88cc09274d3b4
DeazZzis <deazzzis@gmail.com>
2020-03-24 09:10:37 +0200

    Create project

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainWidget());
+
+class MainWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return null;
+  }
+
+}
